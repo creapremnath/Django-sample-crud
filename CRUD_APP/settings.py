12b67da22ef43cb80e5357ae39a9cc39f68fd26c
“creapremnath” <creativepremnath@gmail.com>
2025-12-12 22:26:34 +0530

    added working one

--- CRUD_APP/settings.py
+++ CRUD_APP/settings.py
@@ -60,6 +60,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
     "https://django-sample-crud.onrender.com",
 ]
 
@@ -68,6 +69,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
     "https://django-sample-crud.onrender.com",
 ]
 
