922b93ebc4bb16621dabb24ad14f04d89d48a027
“creapremnath” <creativepremnath@gmail.com>
2025-12-12 23:39:02 +0530

    worked one

--- CRUD_APP/settings.py
+++ CRUD_APP/settings.py
@@ -58,8 +58,8 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://localhost:5173",
-    "https://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     # "http://localhost:5173",
     # "http://127.0.0.1:5173",
     # "http://127.0.0.1:8000",
@@ -69,8 +69,8 @@ CORS_ALLOWED_ORIGINS = [
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://localhost:5173",
-    "https://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     # "http://localhost:5173",
     # "http://127.0.0.1:5173",
     # "http://127.0.0.1:8000",
